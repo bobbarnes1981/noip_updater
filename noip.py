c37e31f96e3ee0516b772f715845bf47525b94e1
Robert Barnes <robertbarnes1981@googlemail.com>
2016-01-05 08:12:28 +0000

    add cron script and exit codes, use classes

--- noip.py
+++ noip.py
@@ -5,25 +5,84 @@ import json
 import sys
 import urllib.parse
 
-client_name = 'RobertBarnesNoIpUpdater'
-client_version = '0.0.1'
-client_email = 'robertbarnes1981@googlemail.com'
+class Message(object):
 
-print(client_name)
-print(client_version)
+    def __init__(self, success, description, exit_code):
+        self.success = success
+        self.description = description
+        self.exit_code = exit_code
 
-messages = {
-    'good': [True, 'DNS hostname update successful.'],
-    'nochg': [True, 'IP address is current, no update performed.'],
-    'nohost': [False, 'Hostname supplied does not exist under specified account.'],
-    'badauth': [False, 'Invalid username password combination.'],
-    'badagent': [False, 'Client disabled.'],
-    '!donator': [False, 'An update request was sent including a feature that is not available to the specified user.'],
-    'abuse': [False, 'Username is blocked due to abuse. Either for not following our update specifications or disabled due to violation of the No-IP terms of service.'],
-    '911': [False, 'A fatal error on our side such as a database outage. Retry the update no sooner than 30 minutes.']
-}
+class Ipify(object):
 
-# https://bugs.gentoo.org/show_bug.cgi?id=261194
+    def __init__(self, url):
+        self.url = url
+
+    def get_ip(self):
+        print('getting ip from: %s' % self.url)
+
+        path = '/?format=json'
+
+        connection = http.client.HTTPSConnection(self.url);
+        connection.request('GET', path)
+        response = connection.getresponse()
+
+        print(response.status)
+
+        if response.status == http.client.OK:
+            data = json.loads(response.readall().decode('ascii'))
+            print(data)
+            return data['ip']
+        return None
+
+class Noip(object):
+
+    client_name = 'RobertBarnesNoIpUpdater'
+    client_version = '0.0.1'
+    client_email = 'robertbarnes1981@googlemail.com'
+
+    def __init__(self, url):
+        self.messages = {
+            'good': Message(True, 'DNS hostname update successful.', 0),
+            'nochg': Message(True, 'IP address is current, no update performed.', 0),
+            'nohost': Message(False, 'Hostname supplied does not exist under specified account.', 1),
+            'badauth': Message(False, 'Invalid username password combination.', 2),
+            'badagent': Message(False, 'Client disabled.', 3),
+            '!donator': Message(False, 'An update request was sent including a feature that is not available to the specified user.', 4),
+            'abuse': Message(False, 'Username is blocked due to abuse. Either for not following our update specifications or disabled due to violation of the No-IP terms of service.', 5),
+            '911': Message(False, 'A fatal error on our side such as a database outage. Retry the update no sooner than 30 minutes.', 6)
+        }
+
+        self.url = url
+
+    def update_ip(self, username, password, hsotname, ip):
+        print('setting ip at: %s' % self.url)
+
+        authorization = base64.b64encode('{username}:{password}'.format(username = username, password = password).encode('ascii')).decode('ascii')
+
+        user_agent = '{client_name}/{client_version} {client_email}'.format(client_name = self.client_name, client_version = self.client_version, client_email = self.client_email)
+
+        headers = {'User-Agent': user_agent, 'Authorization': 'Basic {authorization}'.format(authorization = authorization)}
+
+        path = '/nic/update?hostname={hostname}&myip={myip}'.format(hostname = hostname, myip = myip)
+
+        connection = http.client.HTTPSConnection(self.url)
+        connection.request('GET', path, {}, headers)
+        response = connection.getresponse()
+
+        print(response.status)
+
+        if response.status == http.client.OK:
+            data = response.readall().decode('ascii')
+            print(data)
+    
+            parts = data.split()
+        
+            return self.messages[parts[0]]
+        
+print(Noip.client_name)
+print(Noip.client_version)
+
+# load config
 
 if len(sys.argv) > 1:
     config_file = sys.argv[1]
@@ -35,65 +94,23 @@ print('config file: %s' % config_file)
 config = configparser.ConfigParser()
 config.read(config_file)
 
-myip = None
+username = config.get('noip', 'username')
+password = config.get('noip', 'password')
+hostname = config.get('noip', 'hostname')
 
 # get ip
 
-get_ip_url = 'api.ipify.org'
-
-print('getting ip from: %s' % get_ip_url)
-
-get_ip_path = '/?format=json'
-
-connection = http.client.HTTPSConnection(get_ip_url);
-connection.request('GET', get_ip_path)
-response = connection.getresponse()
-
-print(response.status)
-
-if response.status == http.client.OK:
-    data = json.loads(response.readall().decode('ascii'))
-    print(data)
-
-    myip = data['ip']
+myip = Ipify('api.ipify.org').get_ip()
 
 # update noip
 
 if myip != None:
+    message = Noip('dynupdate.no-ip.com').update_ip(username, password, hostname, myip)
+    if message.success == True:
+        print('SUCCESS')
+    else:
+        print('FAILED')
 
-    set_ip_url = 'dynupdate.no-ip.com'
-
-    print('setting ip at: %s' % set_ip_url)
-
-    username = config.get('noip', 'username')
-    password = config.get('noip', 'password')
-    hostname = config.get('noip', 'hostname')
-
-    authorization = base64.b64encode('{username}:{password}'.format(username = username, password = password).encode('ascii')).decode('ascii')
-
-    user_agent = '{client_name}/{client_version} {client_email}'.format(client_name = client_name, client_version = client_version, client_email = client_email)
-
-    headers = {'User-Agent': user_agent, 'Authorization': 'Basic {authorization}'.format(authorization = authorization)}
-
-    set_ip_path = '/nic/update?hostname={hostname}&myip={myip}'.format(hostname = hostname, myip = myip)
-
-    connection = http.client.HTTPSConnection(set_ip_url)
-    connection.request('GET', set_ip_path, {}, headers)
-    response = connection.getresponse()
-
-    print(response.status)
-
-    if response.status == http.client.OK:
-        data = response.readall().decode('ascii')
-        print(data)
-    
-        parts = data.split()
-        
-        result = messages[parts[0]]
-
-        if result[0] == True:
-            print('SUCCESS %s' % parts[1])
-        else:
-            print('FAILED')
-        print(result[1])
+    print(message.description)
+    sys.exit(message.exit_code)
 
